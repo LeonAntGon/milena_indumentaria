86a74ba475bdb195f30d0283518a92445f6e1482
unknown <gonzalezleonardoantonio056@gmail.com>
2024-05-29 13:39:58 -0300

    adding links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,26 +6,30 @@ export default function Footer() {
 
         <section className="flex flex-col items-center my-10">
             <div>
-                <p className="font-bold cursor-pointer hover:text-green-800 hover:underline">| +54 9 3814 43-2487 </p>
+                <a href="tel:5493812227451" className="font-bold cursor-pointer hover:text-green-800 hover:underline">| +54 9 3812 22-7451 </a>
             </div>
 
             <div className="flex mt-4 ">
                 <div className="pr-4">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                 className="cursor-pointer bg-black  rounded-full py-[5px] icon icon-tabler icon-tabler-brand-facebook hover:stroke-blue-400" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                </svg>
-            </div>
+                    <a href="https://www.facebook.com/share/82mf597b7WkPvfqp/?mibextid=qi2Omg">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                    className="cursor-pointer bg-black  rounded-full py-[5px] icon icon-tabler icon-tabler-brand-facebook hover:stroke-blue-400" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                    </svg>
+                    </a>
+                </div>
 
                 <div>
+                    <a href="https://www.instagram.com/milenaindumentaria0123">
                         <svg xmlns="http://www.w3.org/2000/svg" 
                         className="cursor-pointer bg-black rounded-full py-[5px] icon icon-tabler icon-tabler-brand-instagram hover:stroke-purple-400" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
                         <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
                         <path d="M16.5 7.5l0 .01" />
-                    </svg>
+                        </svg>
+                    </a>
                 </div>
             </div>
         </section>
--- src/app/components/Whatsup.tsx
+++ src/app/components/Whatsup.tsx
@@ -7,7 +7,7 @@ export default function Whatsup(){
             
             <div className='fixed grid place-items-end z-20 bottom-0 right-0 place-content-end rounded-full text-center'>
                 <a className=" align-right  mb-5 mr-5 px-1 py-1"
-                     href="https://api.whatsapp.com/send?phone=5493816090708">
+                     href="https://api.whatsapp.com/send?phone=5493814432487">
                     <Image src={WspLogo} alt='Whats app link' 
                     className='w-14 hover:w-[64px] transition duration-2000 ease-in-out'
                     />
