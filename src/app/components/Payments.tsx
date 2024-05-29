7b27fece9bf63dfa312f0415c961a944158f158a
unknown <gonzalezleonardoantonio056@gmail.com>
2024-05-29 02:39:04 -0300

    no mercado pago

--- src/app/components/Payments.tsx
+++ src/app/components/Payments.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import transfer from "@/public/imgs/assets/transferencia.png";
 import efectivo from "@/public/imgs/assets/efectivo.png";
-import mercadopago from "@/public/imgs/assets/mercadopago.png";
 
 export default function Payments(){
     return(
         <div className="flex justify-center py-4">
 
-            <Image src={mercadopago} className="h-8 w-[60px]" alt="Efectivo" />
             <Image src={transfer} className="h-8 mx-4" alt="Transferencia" />
             <Image src={efectivo} className="h-8 w-[45px] " alt="Efectivo" />
             
